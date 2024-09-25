e9e7bff93f8c00bde28be334269b57f2aa82456f
xuanfishy <148772377+xuanfishy@users.noreply.github.com>
2024-09-25 14:50:11 +0800

    Add files via upload

---
+++ assignment-st-starter.py
@@ -0,0 +1,14 @@
+# import packages
+
+
+# show the title
+
+
+# read csv and show the dataframe
+
+
+# create a figure with three subplots, size should be (15, 5)
+# show the box plot for ticket price with different classes
+# you need to set the x labels and y labels
+# a sample diagram is shown below
+
